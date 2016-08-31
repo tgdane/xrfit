4dae2e1ba62caeb053ead2d0f4040580f0409578
Thomas Dane <thomasgdane@gmail.com>
2016-08-31 16:58:37 +0100

    add README.rst

---
+++ README.rst
@@ -0,0 +1,9 @@
+xrfit: multi-peak fitting for X-ray diffraction data
+====
+
+Library for finding and fitting X-ray diffraction peaks in
+1D data.
+
+----
+
+
--- setup.py
+++ setup.py
@@ -1,17 +1,40 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-packages=[
-	'',
-	'xrfit',
-]
-package_dir = {'':'lib'}
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages=find_packages(exclude=['docs', 'tests'])
+
+# double check classifiers on https://pypi.python.org/pypi?%3Aaction=list_classifiers
+classifiers = ["Development Status :: 3 - alpha",
+               "Intended Audience :: Developers",
+               "Programming Language :: Python :: 2",
+               "Programming Language :: Python :: 2.7",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: GNU General Public License (GPL)",
+               "License :: OSI Approved :: MIT License",
+               "Topic :: Software Development :: Libraries :: Python Modules",
+               "Operating System :: Microsoft :: Windows",
+               "Operating System :: Unix",
+               "Operating System :: MacOS :: MacOS X",
+               "Topic :: Scientific/Engineering :: Physics"
+               ]
 
 setup(
-	name          =   "xrfit",
-	version       =   "0.1.0",
-	description   =   "Fitting routines for X-ray diffraction peaks",
-	author        =   "Thomas Dane",
-	author_email  =   "dane@esrf.fr",
-	packages      =   packages,
-	package_dir   =   package_dir,
-)
+    name='xrfit',
+    version='0.1.0',    
+    description='Fitting of X-ray diffraction/scattering data',
+    long_description=long_description,
+    url='https://github.com/tgdane/xrfit',
+    author='Thomas Dane',
+    author_email='thomasgdane@gmail.com',
+    licence="GPL",
+    classifiers=classifiers,
+    packages=packages
+    )
